0007f25cdcedd5ad340893e97f9da1f3b804e3d6
aspiringguru <bmatthewtaylor@gmail.com>
2017-08-30 13:07:55 +1000

    AlbumList converted from functional component to class component

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,16 +1,22 @@
 //import libraries for making a components
-import React  from 'react';
+import React, { Component }  from 'react';
 import { Text, View } from 'react-native';
 
-const AlbumList = () => {
+//const AlbumList = () => {
+//replace previous functional component with class component
+//render method returns valid JSX
+class AlbumList extends Component {
+  render() {
     return (
       <View>
-        <Text>Album List!!!</Text>
-        <Text>222!</Text>
-        <Text>333</Text>
+        <Text>Album List</Text>
+        <Text>222!-------</Text>
+        <Text>333-------</Text>
       </View>
     );
-};
+  }
+}
+
 
 //make the component available to other parts of the app
 export default AlbumList;
@@ -18,6 +24,6 @@ export default AlbumList;
 /*
 *   functional component
 *   class component
-*   
+*
 *
 */
