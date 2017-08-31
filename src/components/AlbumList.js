d6b67b851f330ca2005a4edce8a0d328cb630fcb
aspiringguru <bmatthewtaylor@gmail.com>
2017-08-31 18:08:01 +1000

    json loaded, state added for class based component ch37

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -5,22 +5,33 @@ import { Text, View } from 'react-native';
 //const AlbumList = () => {
 //replace previous functional component with class component
 //render method returns valid JSX
+//only use state with class based Components.
 class AlbumList extends Component {
-
+  //add a class level property and set to object
+  //with key = 'albums' & value set to empty array
+  state = { albums: [] };
   /*
   *   when component is about to be rendered to screen
   *   componentWillMount is executed.
+  *   need to deal with asynchronous data loading.
+  *   need to deal with changing state.
+  *   setState causes re-rendering when response received from url
   */
   componentWillMount() {
     console.log('componentWillMount in AlbumList - start');
     fetch('https://rallycoding.herokuapp.com/api/music_albums')
       .then((response) => response.json())
-      .then((response) => console.log(response));
-      //.then((response) => response.json())
-      //.then((responseData) => this.setState({ albums: responseData }));
+      .then((response) => this.setState({ albums: response }));
+      //.then((response) => this.setState({ albums: response.data }));//this gave errors.
+      //.then((response) => console.log(response));
+      //  nb: pass in object {albums: response.data}
+      //  has key 'albums', value response.data
+      //
     console.log('componentWillMount in AlbumList - end');
   }
   render() {
+    console.log(this.state);
+    console.log(typeof this.state);
     return (
       <View>
         <Text>Album List</Text>
