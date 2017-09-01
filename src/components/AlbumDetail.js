e589089d496a8e25f9cbebb43fd9ff5724798401
aspiringguru <bmatthewtaylor@gmail.com>
2017-09-01 14:49:20 +1000

    images and styles added.

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -13,21 +13,28 @@ import CardSection from './CardSection';
 *   which enables <Text>{props.album.title}</Text>
 *   to become <Text>{album.title}</Text>
 *   NB: styles.thumbnailStyle required to pass height & width for image
+*   destructuring : ie const { a,b,c } = styles replaces styles.a, styles.b etc.
 */
 const AlbumDetail = ({album}) => {
   const { title, artist, thumbnail_image } = album;
-  const { thumbnailStyle, headerContentStyle } = styles;
+  const {
+    headerTextStyle,
+    thumbnailStyle,
+    headerContentStyle,
+    thumbnailContainerStyle,
+    imageStyle
+  } = styles;
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle}>
           <Image
             style={thumbnailStyle}
             source={{ uri: thumbnail_image }}
           />
         </View>
         <View style={headerContentStyle}>
-          <Text>{title}</Text>
+          <Text style={headerTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
       </CardSection>
@@ -40,9 +47,24 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-around'
   },
+  headerTextStyle: {
+    fontSize: 18,
+    color: 'red',
+  },
   thumbnailStyle: {
     height: 50,
     width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
 };
 
