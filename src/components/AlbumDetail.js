9549e1df1cdfff922f1020678179ebe5d5dc17b3
aspiringguru <bmatthewtaylor@gmail.com>
2017-09-01 14:06:20 +1000

    added thumbnail images ch 46

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 
@@ -7,12 +7,25 @@ import CardSection from './CardSection';
 *   consume the prop passed to this
 *   ch41: Card replaces View in previous version.
 *   transfer props to Card
+*   TODO
+*   nb: const AlbumDetail = (props) =>
+*   changed to const AlbumDetail = ({ album }) =>
+*   which enables <Text>{props.album.title}</Text>
+*   to become <Text>{album.title}</Text>
+*
 */
-const AlbumDetail = (props) => {
+const AlbumDetail = ( props ) => {
+//  const { title, artist, thumbnail_image } = album;
   return (
     <Card>
       <CardSection>
         <View>
+          <Image
+            style={styles.thumbnailStyle}
+            source={{ uri: props.album.thumbnail_image }}
+          />
+        </View>
+        <View style={styles.headerContentStyle}>
           <Text>{props.album.title}</Text>
           <Text>{props.album.artist}</Text>
         </View>
